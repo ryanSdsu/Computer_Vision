840e2bfaa1f8eeebb84722df8a97c8099e329259
Ryan Ragasa <RJ@ryans-imac.home>
2018-06-17 18:27:17 -0400

    Homework 2 updated

--- Homework_Two/HomeworkTwo.py
+++ Homework_Two/HomeworkTwo.py
@@ -1,6 +1,9 @@
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
+import os
+from numpy import zeros
+
 
 #Get image one to show in black and white
 img1 = cv2.imread('/Users/RJ/PycharmProjects/Computer_Vision/Homework_Two/cat.bmp', 0)
@@ -103,3 +106,89 @@ and then just divide by 2 i.e. "finalImgB = finalImgB / 2"
 
 From here merge them together (finalImgB, finalImgG, finalImgR) to create a blended image!
 """
+def imageBlender (lowFilterImageUrl, highFilterImageUrl, sizeOfFilter):
+
+    lowFilterImage = cv2.imread(lowFilterImageUrl)
+    lowFilterImage = cv2.cvtColor(lowFilterImage, cv2.COLOR_BGR2RGB)
+
+    plt.imshow(lowFilterImage)
+    plt.show()
+
+    highFilterImage = cv2.imread(highFilterImageUrl)
+    highFilterImage = cv2.cvtColor(highFilterImage, cv2.COLOR_BGR2RGB)
+
+    plt.imshow(highFilterImage)
+    plt.show()
+
+    redLowFilterImageOriginal = lowFilterImage[:,:,0]
+    greenLowFilterImageOriginal = lowFilterImage[:,:,1]
+    blueLowFilterImageOriginal = lowFilterImage[:,:,2]
+
+    redHighFilterImageOriginal = highFilterImage[:,:,0]
+    greenHighFilterImageOriginal = highFilterImage[:,:,1]
+    blueHighFilterImageOriginal = highFilterImage[:,:,2]
+
+    def lowFilterTransform(image):
+        #FOURIER IMAGE
+        img1_f = np.fft.fft2(image)
+        img1_shift = np.fft.fftshift(img1_f)
+        #Get the total # of rows/columns and the middle of both
+        totalRows = np.size(img1_f, 0)
+        totalColumns = np.size(img1_f,1)
+        centerRows = int(totalRows/2)
+        centerColumns = int(totalColumns/2)
+        #Play around with this number as it adjusts the size of the filter contribution of Image One and Image Two
+        n = sizeOfFilter
+        #IMAGE ONE GET THE LOW FILTER
+        tempFrame = np.zeros_like(img1_shift)
+        tempFrame[centerRows-n:centerRows+n, centerColumns-n:centerColumns+n] = img1_shift[centerRows - n:centerRows+n, centerColumns-n:centerColumns+n]
+        #IMAGE ONE w/ THE LOW FILTER, return back to original
+        inverse_tempframe_One = np.fft.ifft2(tempFrame)
+        inverse_tempframe_One_abs = np.abs(inverse_tempframe_One)
+        return inverse_tempframe_One_abs
+
+    redLowFilterImage = lowFilterTransform(redLowFilterImageOriginal)
+    greenLowFilterImage = lowFilterTransform(greenLowFilterImageOriginal)
+    blueLowFilterImage = lowFilterTransform(blueLowFilterImageOriginal)
+
+    def highFilterTransform(image):
+        #FOURIER IMAGE
+        img1_f = np.fft.fft2(image)
+        img1_shift = np.fft.fftshift(img1_f)
+        #Get the total # of rows/columns and the middle of both
+        totalRows = np.size(img1_f, 0)
+        totalColumns = np.size(img1_f,1)
+        centerRows = int(totalRows/2)
+        centerColumns = int(totalColumns/2)
+        #Play around with this number as it adjusts the size of the filter contribution of Image One and Image Two
+        n = sizeOfFilter
+        #IMAGE TWO GET THE HIGH FILTER
+        #From the fourier image two, edit it so that we can get a low frequency which is an image with a siloutte by blocking out the outside
+        tempFrame = np.copy(img1_shift)
+        tempFrame[centerRows-n:centerRows+n, centerColumns-n:centerColumns+n] = 0
+        #IMAGE TWO w/ THE HIGH FILTER, return back to original
+        #Get the inverse now of the fourier image one to turn back to original image one (with now a siloutte)
+        inverse_tempframe_Two = np.fft.ifft2(tempFrame_Two)
+        inverse_tempframe_Two_abs = np.abs(inverse_tempframe_Two)
+        return inverse_tempframe_Two_abs
+
+    redHighFilterImage = highFilterTransform(redHighFilterImageOriginal)
+    greenHighFilterImage = highFilterTransform(greenHighFilterImageOriginal)
+    blueHighFilterImage = highFilterTransform(blueHighFilterImageOriginal)
+
+    rBlend = (redLowFilterImage + redHighFilterImage) / 2
+    gBlend = (greenLowFilterImage + greenHighFilterImage) / 2
+    bBlend = (blueLowFilterImage + blueHighFilterImage) / 2
+
+    rBlend = rBlend.astype(np.uint8)
+    gBlend = gBlend.astype(np.uint8)
+    bBlend = bBlend.astype(np.uint8)
+
+    im3 = cv2.merge((rBlend,gBlend,bBlend))
+    plt.imshow(im3)
+    plt.show()
+
+    return
+
+imageBlender("/Users/RJ/PycharmProjects/Computer_Vision/Homework_Two/cat.bmp",
+             "/Users/RJ/PycharmProjects/Computer_Vision/Homework_Two/dog.bmp",15)
