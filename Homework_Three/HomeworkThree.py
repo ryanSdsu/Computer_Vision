8ece38becb0d2c101c2dd789c459f6680b8f5995
Ryan Ragasa <RJ@ryans-imac.home>
2018-06-27 21:01:33 -0400

    Cleaned up the code for the last 3 definitions

--- Homework_Three/HomeworkThree.py
+++ Homework_Three/HomeworkThree.py
@@ -52,24 +52,6 @@ def correlation(originalImage, croppedImage):
     print("Size of cropped eye height: {}".format(hEye))
     print("Size of cropped eye width: {}".format(wEye))
 
-    #This gets the cropped size of the original image and grabs via the eye
-    croppedImg1 = img1Original[0:hEye, 0:wEye]
-
-    #This is an auto built function which grabs the correlation through cv2 and is what we're tring to get
-    # cor = cv2.matchTemplate(img1Original,img1Eye,cv2.TM_CCORR_NORMED)
-    # minValue, maxValue, minLocation, maxLocation = cv2.minMaxLoc(cor)
-    # print("Location of cor eye height: {}".format(maxLocation[0]))
-    # print("Location of cor eye width: {}".format(maxLocation[1]))
-    # plt.imshow(cor, cmap='gray')
-    # plt.show()
-
-    #Create a custom sliding window (i.e. do the correlation from scratch )
-    ###
-    #This gets the cropped size of the image via Test check
-    # croppedImgTest = img1Original[111:111+hEye, 105:105+wEye]
-    # plt.imshow(croppedImgTest, cmap='gray')
-    # plt.show()
-
     dtype = [('score', float), ('height', int), ('width', int)]
     maxList = []
 
@@ -86,15 +68,6 @@ def correlation(originalImage, croppedImage):
             #This does the "Correlation" i.e. Q1
             correlationValue = pearsonr(flattenedCroppedImg1, flattenedImg1Eye)
             correlationValue = correlationValue[0] * 255
-
-            # #This does the "SSD" i.e. Q2
-            # correlationValue = ((flattenedCroppedImg1-flattenedImg1Eye)**2).sum()
-            # # This is how you get the 31046
-            # # if correlationValue > max:
-            # #     max = correlationValue
-            # correlationValue = 1 - float(correlationValue / 31046)
-            # correlationValue = float(correlationValue * 255)
-
 
             tempImage1[heightIndex][widthIndex] = correlationValue
             maxList.append((correlationValue, heightIndex, widthIndex))
